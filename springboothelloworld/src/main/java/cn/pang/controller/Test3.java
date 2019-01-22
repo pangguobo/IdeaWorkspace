61add15425215f16e854bc386b5caf7522a5d006
pangguobo <pangguobo@163.com>
2019-01-22 21:25:20 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	springboothelloworld/src/main/java/cn/pang/controller/Test3.java

--- springboothelloworld/src/main/java/cn/pang/controller/Test3.java
+++ springboothelloworld/src/main/java/cn/pang/controller/Test3.java
@@ -3,7 +3,7 @@ package cn.pang.controller;
 public class Test3 {
     public static void main(String[] args) {
 
-        System.out.println("明天加班吧");
+        System.out.println("自己感觉只能加班了");
 
 
     }
