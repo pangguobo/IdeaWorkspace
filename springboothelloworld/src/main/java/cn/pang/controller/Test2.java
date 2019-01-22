633699136e69be6e1186e3a080d3f41924f4557e
pangguobo <pangguobo@163.com>
2019-01-22 20:56:00 +0800

    2222

--- springboothelloworld/src/main/java/cn/pang/controller/Test2.java
+++ springboothelloworld/src/main/java/cn/pang/controller/Test2.java
@@ -3,6 +3,10 @@ package cn.pang.controller;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("这是今天写的一个功能");
+        System.out.println("这是今天写的一个功能");
+        System.out.println("这是今天写的一个功能");
+        System.out.println("这是今天写的一个功能");
+        System.out.println("这是今天写的一个功能");
 
         ///打飞机的看法就看见
 
