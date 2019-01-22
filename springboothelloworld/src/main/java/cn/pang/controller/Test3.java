b25014375e41702cd153716ed6f70c504c70de69
pangguobo <46235127+pangguobo@users.noreply.github.com>
2019-01-22 21:27:15 +0800

    888
    8888

--- springboothelloworld/src/main/java/cn/pang/controller/Test3.java
+++ springboothelloworld/src/main/java/cn/pang/controller/Test3.java
@@ -3,8 +3,8 @@ package cn.pang.controller;
 public class Test3 {
     public static void main(String[] args) {
 
-        System.out.println("明天加班吧");
-        System.out.println("后天休息吧,夜夜");
+        System.out.println("这是一个命令行吧");
+       
 
 
     }
