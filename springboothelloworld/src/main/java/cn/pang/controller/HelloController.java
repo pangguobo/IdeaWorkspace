be2d548d6cdcfd44772089911cfc8b5e97a0ed2e
pangguobo <pangguobo@163.com>
2019-01-22 20:27:23 +0800

    首次提交

---
+++ springboothelloworld/src/main/java/cn/pang/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cn.pang.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class HelloController {
+
+    @RequestMapping
+    public String uu(){
+        System.out.println("1212");
+        return "uu";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class,args);
+    }
+}
