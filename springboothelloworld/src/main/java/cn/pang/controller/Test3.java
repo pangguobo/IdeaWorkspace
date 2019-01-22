41c449285fc656a26a19b3bbbdbc6acebd94892a
pangguobo <46235127+pangguobo@users.noreply.github.com>
2019-01-22 21:11:25 +0800

    55
    55

--- springboothelloworld/src/main/java/cn/pang/controller/Test3.java
+++ springboothelloworld/src/main/java/cn/pang/controller/Test3.java
@@ -2,6 +2,6 @@ package cn.pang.controller;
 
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("tset3dfdfdfdfdffdf");
+        System.out.println("明天继续工作了");
     }
 }
