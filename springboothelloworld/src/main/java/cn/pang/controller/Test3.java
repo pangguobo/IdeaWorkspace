1b9d57f162d9ada3aad54d64bb2156b21eff7f1e
pangguobo <pangguobo@163.com>
2019-01-23 08:19:52 +0800

    Merge branch 'master' of https://github.com/pangguobo/IdeaWorkspace
    # Conflicts:
    #	springboothelloworld/src/main/java/cn/pang/controller/Test3.java
    #	springboothelloworld/src/main/java/cn/pang/controller/test.xml

--- springboothelloworld/src/main/java/cn/pang/controller/Test3.java
+++ springboothelloworld/src/main/java/cn/pang/controller/Test3.java
@@ -3,9 +3,7 @@ package cn.pang.controller;
 public class Test3 {
     public static void main(String[] args) {
 
-        System.out.println("加油每一天吧孩子");
-
-        System.out.println("这是一个命令行吧");
+        System.out.println("ggndakj kjdk j ");
 
 
 
